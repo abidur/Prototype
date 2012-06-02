026051ed0728ad13ed1203313e0e844e5966bd46
Abid Rahman <abidur@hotmail.com>
2012-06-02 07:22:41 -0500

    add

--- YelpTest/Controllers/MyDayController.cs
+++ YelpTest/Controllers/MyDayController.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
+using System.Web;
 using System.Web.Http;
+using System.Xml.Linq;
 using YelpSharp;
 using YelpSharp.Data;
 using YelpSharp.Data.Options;
@@ -16,28 +18,46 @@ namespace YelpTest.Controllers
         // GET /api/default1
         public IList<Agenda> GetAll(double lat, double lon)
         {
-            Yelp yelp = new Yelp(WebApiApplication.YelpOptions);
             //var results = yelp.Search("coffee", "kansas city");
 
+            var categoriesXML = XDocument.Load(HttpContext.Current.Server.MapPath(@"\categories.xml"));
+            //var BreakFastCategories = categoriesXML.Descendants("category").Where(a => (string)a.Attribute("yelpName") == "breakfast_brunch");
+
+            var userAgenda = new UserAgenda();
+            userAgenda.Agendas = new List<Agenda>();
+            var getBreakfastOptions = GetSearchResults(lat, lon, "breakfast_brunch");
+
+            var randomIndex = new Random().Next(20);
+            userAgenda.Agendas.Add(new Agenda { Business = getBreakfastOptions.businesses[randomIndex], TimeSlot = 8, TimeString = Agenda.TimeLine[8], AllOtherOptionsIntheSameCategory = getBreakfastOptions.businesses.Where((b, i) => i != randomIndex).ToList() });
+            var tenAmCategories = string.Join(",", categoriesXML.Descendants("category").Where(a => (int)a.Element("time") == 10).Select(a => a.Attribute("yelpName")).Select(b => b.Value).ToArray());
+            var tenAmOptions = GetSearchResults(lat, lon, tenAmCategories);
+            userAgenda.Agendas.Add(new Agenda { Business = tenAmOptions.businesses[randomIndex], TimeSlot = 10, TimeString = Agenda.TimeLine[10], AllOtherOptionsIntheSameCategory = tenAmOptions.businesses.Where((b, i) => i != randomIndex).ToList() });
+            var lunchOptions = GetSearchResults(lat, lon, "restaurants");
+            userAgenda.Agendas.Add(new Agenda { Business = getBreakfastOptions.businesses[randomIndex], TimeSlot = 18, TimeString = Agenda.TimeLine[12], AllOtherOptionsIntheSameCategory = lunchOptions.businesses.Where((b, i) => i != randomIndex).ToList() });
+            var fifteenCategories = string.Join(",", categoriesXML.Descendants("category").Where(a => (int)a.Element("time") == 15).Select(a => a.Attribute("yelpName")).Select(b => b.Value).ToArray());
+            var fifteenOptions = GetSearchResults(lat, lon, tenAmCategories);
+            userAgenda.Agendas.Add(new Agenda { Business = fifteenOptions.businesses[randomIndex], TimeSlot = 15, TimeString = Agenda.TimeLine[15], AllOtherOptionsIntheSameCategory = fifteenOptions.businesses.Where((b, i) => i != randomIndex).ToList() });
+            var dinnerOptions = GetSearchResults(lat, lon, "restaurants");
+            userAgenda.Agendas.Add(new Agenda { Business = dinnerOptions.businesses[randomIndex], TimeSlot = 18, TimeString = Agenda.TimeLine[18], AllOtherOptionsIntheSameCategory = dinnerOptions.businesses.Where((b, i) => i != randomIndex).ToList() });
+            var nightCategories = string.Join(",", categoriesXML.Descendants("category").Where(a => (int)a.Element("time") == 22).Select(a => a.Attribute("yelpName")).Select(b => b.Value).ToArray());
+            var nightOptions = GetSearchResults(lat, lon, tenAmCategories);
+            userAgenda.Agendas.Add(new Agenda { Business = nightOptions.businesses[randomIndex], TimeSlot = 22, TimeString = Agenda.TimeLine[22], AllOtherOptionsIntheSameCategory = nightOptions.businesses.Where((b, i) => i != randomIndex).ToList() });
+            return userAgenda.Agendas;
+        }
+
+        private static SearchResults GetSearchResults(double lat, double lon, string categories)
+        {
+            Yelp yelp = new Yelp(WebApiApplication.YelpOptions);
             var results = yelp.Search(new SearchOptions
             {
-                GeneralOptions = new GeneralOptions { category_filter = "food", radius_filter = 30000, term = "coffee", limit=6, offset=0 , sort=2, deals_filter=false},
+                GeneralOptions = new GeneralOptions { category_filter = categories, radius_filter = 30000, term = "", limit = 20, offset = 0, sort = 2, deals_filter = false },
                 LocationOptions = new CoordinateOptions
                 {
                     latitude = lat,
                     longitude = lon
                 }
             });
-
-            UserAgenda uAgenda=new UserAgenda();
-            int i = 0;
-            uAgenda.Agendas = new List<Agenda>();
-            foreach (var result in results.businesses)
-            {
-                uAgenda.Agendas.Add(new Agenda { Business = result, TimeSlot = ++i, TimeString=Agenda.TimeLine[++i] });
-            }
-
-            return uAgenda.Agendas;
+            return results;
         }
 
         // GET /api/default1/5
--- YelpTest/Models/Agenda.cs
+++ YelpTest/Models/Agenda.cs
@@ -11,22 +11,18 @@ namespace YelpTest.Models
         public Business Business { get; set; }
 
         public int TimeSlot { get; set; }
+
         public string TimeString { get; set; }
 
         public static readonly Dictionary<int, string> TimeLine = new Dictionary<int, string>{
-            {1,"8am to 9am"},
-            {2,"9am to 10am"},
-            {3,"10am to 11am"},
-            {4,"12pm to 1pm"},
-            {5,"1pm to 2pm"},
-            {6,"2pm to 3pm"},
-            {7,"3pm to 4pm"},
-            {8,"4pm to 5pm"},
-            {9,"5pm to 6pm"},
-            {10,"7pm to 8pm"},
-            {11,"8pm to 9pm"},
-            {12,"10pm to 11pm"},
-            {13,"11pm to 12am"}
+            {8,"Breakfast"},
+            {10,"Activites for 10am"},
+            {12,"Lunch"},
+            {15,"Activities for 3pm"},
+            {18,"Dinner"},
+            {22,"Nightlife"},
         };
+
+        public List<YelpSharp.Data.Business> AllOtherOptionsIntheSameCategory { get; set; }
     }
 }
